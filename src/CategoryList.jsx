0ffb0e14b3cd575868383e055446aecf4d5a7f04
Shreya904 <shreyasrivastava92006@gmail.com>
2024-08-13 14:33:01 +0530

    arrow fixed

--- src/CategoryList.jsx
+++ src/CategoryList.jsx
@@ -1,46 +1,46 @@
-import React, { forwardRef } from "react";
-import { IoMdArrowDropleftCircle } from "react-icons/io";
-import { IoMdArrowDroprightCircle } from "react-icons/io";
+import React from "react";
+import {
+  IoMdArrowDropleftCircle,
+  IoMdArrowDroprightCircle,
+} from "react-icons/io";
 
-const CategoryList = forwardRef(
-  ({ categories, onClickCategory, activeCategory }, ref) => {
-    const handleScroll = (direction) => {
-      const container = ref.current;
-      if (container) {
-        const scrollAmount = direction === "right" ? 100 : -100;
-        container.scrollBy({ left: scrollAmount, behavior: "smooth" });
-      }
-    };
+const CategoryList = ({ categories, onClickCategory, activeCategory }) => {
+  const handleScroll = (direction) => {
+    const container = document.querySelector(".category-list");
+    if (container) {
+      const scrollAmount = direction === "right" ? 100 : -100;
+      container.scrollBy({ left: scrollAmount, behavior: "smooth" });
+    }
+  };
 
-    return (
-      <div className="white">
-        <div className="heading">
-          <h2>Select Services</h2>
+  return (
+    <div className="white">
+      <div className="heading">
+        <h2>Select Services</h2>
+      </div>
+      <div className="category-list-container">
+        <div className="category-list">
+          {categories.map((category, index) => (
+            <button
+              key={index}
+              className={`category-button ${
+                activeCategory === category ? "active" : ""
+              }`}
+              onClick={() => onClickCategory(category)}
+            >
+              {category}
+            </button>
+          ))}
         </div>
-        <div className="category-list-container">
-          <div ref={ref} className="category-list">
-            {categories.map((category, index) => (
-              <button
-                key={index}
-                className={`category-button ${
-                  activeCategory === category ? "active" : ""
-                }`}
-                onClick={() => onClickCategory(category)}
-              >
-                {category}
-              </button>
-            ))}
-          </div>
-          <div className="arrow-left" onClick={() => handleScroll("left")}>
-            <IoMdArrowDropleftCircle />
-          </div>
-          <div className="arrow-right" onClick={() => handleScroll("right")}>
-            <IoMdArrowDroprightCircle />
-          </div>
+        <div className="arrow-left" onClick={() => handleScroll("left")}>
+          <IoMdArrowDropleftCircle />
+        </div>
+        <div className="arrow-right" onClick={() => handleScroll("right")}>
+          <IoMdArrowDroprightCircle />
         </div>
       </div>
-    );
-  }
-);
+    </div>
+  );
+};
 
 export default CategoryList;
